f6694eaa398c2ea5025cbd8c4ecdabf3df30ddd0
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-10-21 18:49:37 +0200

    fix(lab01): fixed errors in lab01, now it's fully working

--- Lab 01 - RRZ - metoda Eulera, RK2, RK4/Program/IMN-lab01.cpp
+++ Lab 01 - RRZ - metoda Eulera, RK2, RK4/Program/IMN-lab01.cpp
@@ -1,31 +1,106 @@
-﻿#include <array>
-#include <cmath>
-#include <string>
-#include <vector>
+﻿#include <cmath>
+#include <cstdio>
 
-#define M_PI 3.14
+//w sumie mogłem korzystać C skoro i tak korzystałem z funkcji z C, ale juz niech tak zostanie
+//#define M_PI 3.14
+//potrzebne w VS, na taurusie niepotrzebne 
 
 using namespace std;
 
+//PROTOTYPY FUNKCJI
+double analytical_sol(double, double);
+void ex1_euler(FILE*, FILE*, double);
+void ex1_euler_analytically(FILE*, double);
+
+void ex2_rk2(FILE*, FILE*, double);
+
+double rk4_k1(double, double);
+double rk4_k2(double, double, double, double);
+double rk4_k3(double, double, double, double);
+double rk4_k4(double, double, double, double);
+
+void ex3_rk4(FILE* solutions, FILE*, double);
+
+double V_t(double om_v, double t);
+double g(double, double, double, double, double, double, double);
+void ex4_rlc(double, FILE*, FILE*, double);
+
+int main() {
+
+    FILE* solutions;
+    FILE* errors;
+    FILE* analytical;
+
+    solutions = fopen("ex1_sol.txt", "w");
+    errors = fopen("ex1_err.txt", "w");
+    analytical = fopen("ex1_an.txt", "w");
+
+    //wywołanie funkcji dla zadania 1
+    ex1_euler(solutions, errors, 0.01);
+    ex1_euler(solutions, errors, 0.1);
+    ex1_euler(solutions, errors, 1.0);
+
+    ex1_euler_analytically(analytical, 0.01);
+
+    fclose(solutions);
+    fclose(errors);
+    fclose(analytical);
+
+    //wywołanie funkcji dla zadania 2
+    solutions = fopen("ex2_sol.txt", "w");
+    errors = fopen("ex2_err.txt", "w");
+
+    ex2_rk2(solutions, errors, 0.01);
+    ex2_rk2(solutions, errors, 0.1);
+    ex2_rk2(solutions, errors, 1.0);
+
+    fclose(solutions);
+    fclose(errors);
+
+    //wywołanie funkcji dla zadania 3
+    solutions = fopen("ex3_sol.txt", "w");
+    errors = fopen("ex3_err.txt", "w");
+
+    ex3_rk4(solutions, errors, 0.01);
+    ex3_rk4(solutions, errors, 0.1);
+    ex3_rk4(solutions, errors, 1.0);
+
+    fclose(solutions);
+    fclose(errors);
+
+    //wywołanie funkcji dla zadania 4
+    FILE* rlcQ;
+    FILE* rlcI;
+    
+    rlcQ = fopen("ex4_Q.txt", "w");
+    rlcI = fopen("ex4_I.txt", "w");
+
+    double dt = pow(10.0, -4.0);
+
+    ex4_rlc(0.5, rlcQ, rlcI, dt);
+    ex4_rlc(0.8, rlcQ, rlcI, dt);
+    ex4_rlc(1.0, rlcQ, rlcI, dt);
+    ex4_rlc(1.2, rlcQ, rlcI, dt);
+
+    fclose(rlcQ);
+    fclose(rlcI);
+}
+
 // Zadanie 1. //////////////////////// EULER /////////////////////////////
 
-double FUNCTIONX(double lambda, double t) {
+double analytical_sol(double lambda, double t) {
     return exp(lambda * t);
 }
 
 void ex1_euler(FILE* solutions, FILE* errors, double dt) {
-    //początek i koniec naszego zakresu
-    double t_min = 0.0;
-    double t_max = 5.0;
     double lambda = -1.;
-
     double y = 1.;
 
-    for (double t = t_min; t <= t_max; t += dt) {
-        y = y + dt * (lambda * y);      
-
+    for (double t = 0.; t <= 5.; t += dt) {
         fprintf(solutions, "%f %f \n", t, y);
-        fprintf(errors, "%f %f \n", t, y - FUNCTIONX(lambda, t));
+        fprintf(errors, "%f %f \n", t, y - analytical_sol(lambda, t));
+
+        y = y + dt * (lambda * y);
     }
 
     //robimy przerwę w plikach
@@ -34,15 +109,11 @@ void ex1_euler(FILE* solutions, FILE* errors, double dt) {
 }
 
 void ex1_euler_analytically(FILE* filename, double dt) {
-    //początek i koniec naszego zakresu
-    double t_min = 0.0;
-    double t_max = 5.0;
     double lambda = -1.;
-
     double y = 1.;
 
-    for (double t = t_min; t <= t_max; t += dt) {
-        fprintf(filename, "%f %f \n", t, FUNCTIONX(lambda, t));
+    for (double t = 0.; t <= 5.; t += dt) {
+        fprintf(filename, "%f %f \n", t, analytical_sol(lambda, t));
     }
 
     //robimy przerwę w pliku
@@ -52,123 +123,71 @@ void ex1_euler_analytically(FILE* filename, double dt) {
 // Zadanie 2. //////////////////////// RK2 /////////////////////////////
 
 void ex2_rk2(FILE* solutions, FILE* errors, double dt) {
-    double t_min = 0.0;
-    double t_max = 5.0;
     double lambda = -1.;
-
     double y = 1.;
-  
 
-    for (double t = t_min; t <= t_max; t += dt) {
+    for (double t = 0.; t <= 5.; t += dt) {
+
         double k_1 = lambda * y;
         double k_2 = lambda * (y + dt * k_1);
 
-        y = y + (dt / 2.) * (k_1 + k_2);
-
         fprintf(solutions, "%f %f \n", t, y);
-        fprintf(errors, "%f %f \n", t, y - FUNCTIONX(lambda, t));
+        fprintf(errors, "%f %f \n", t, y - analytical_sol(lambda, t));
+
+        y = y + (dt / 2.) * (k_1 + k_2);
     }
 
     fprintf(solutions, " \n \n");
     fprintf(errors, " \n \n");
 }
 
-//wiem, ta funkcja jest taka sama jak z przykładu pierwszego, 
-//lecz po prostu chciałem żeby się jakoś wyróżniały
-void ex2_rk2_analytically(FILE* filename, double dt) {
-    //początek i koniec naszego zakresu
-    double t_min = 0.0;
-    double t_max = 5.0;
-    double lambda = -1.;
-
-    double y = 1.;
-
-    for (double t = t_min; t <= t_max; t += dt) {
-        fprintf(filename, "%f %f \n", t, FUNCTIONX(lambda, t));
-    }
-
-    //robimy przerwę w pliku
-    fprintf(filename, " \n \n");
-}
-
 // Zadanie 3. //////////////////////// RK4 /////////////////////////////
 
-//funckja służąca do obliczenia k_1
-double rk4_k1(double lambda, double y) {
-    return lambda * y;
-}
-
-//funckja służąca do obliczenia k_2
-double rk4_k2(double lambda, double y, double dt, double k_1) {
-    return lambda * (y + dt * k_1 / 2.0);
-}
-
-//funckja służąca do obliczenia k_3
-double rk4_k3(double lambda, double y, double dt, double k_2) {
-    return lambda * (y + dt * k_2 / 2.0);
-}
-
-//funckja służąca do obliczenia k_4
-double rk4_k4(double lambda, double y, double dt, double k_3) {
-    return lambda * (y + dt * k_3);
-}
-
-
 void ex3_rk4(FILE* solutions, FILE* errors, double dt) {
-    //początek i koniec naszego zakresu
-    double t_min = 0.0;
-    double t_max = 5.0;
     double lambda = -1.;
-
     double y = 1.;
 
-    for (double t = t_min; t <= t_max; t += dt) {
+    for (double t = 0.; t <= 5.; t += dt) {
 
         double k_1 = rk4_k1(lambda, y);
         double k_2 = rk4_k2(lambda, y, dt, k_1);
         double k_3 = rk4_k3(lambda, y, dt, k_2);
         double k_4 = rk4_k4(lambda, y, dt, k_3);
 
-        y = y + (dt / 6.) * (k_1 + (2 * k_2) + (2 * k_3) + k_4);
-
         fprintf(solutions, "%f %f \n", t, y);
-        fprintf(errors, "%f %f \n", t, y - FUNCTIONX(lambda, t));
-        
+        fprintf(errors, "%f %f \n", t, y - analytical_sol(lambda, t));
+
+        y = y + (dt / 6.) * (k_1 + (2 * k_2) + (2 * k_3) + k_4);
     }
 
     fprintf(solutions, " \n \n");
     fprintf(errors, " \n \n");
 }
 
-//wiem, ta funkcja jest taka sama jak z przykładu pierwszego, 
-//lecz po prostu chciałem żeby się jakoś wyróżniały
-void ex3_rk4_analytically(FILE* filename, double dt) {
-    //początek i koniec naszego zakresu
-    double t_min = 0.0;
-    double t_max = 5.0;
-    double lambda = -1.;
+//funkcja służąca do obliczenia k_1
+double rk4_k1(double lambda, double y) {
+    return lambda * y;
+}
 
-    double y = 1.;
+//funkcja służąca do obliczenia k_2
+double rk4_k2(double lambda, double y, double dt, double k_1) {
+    return lambda * (y + dt * k_1 / 2.0);
+}
 
-    for (double t = t_min; t <= t_max; t += dt) {
-        fprintf(filename, "%f %f \n", t, FUNCTIONX(lambda, t));
-    }
+//funkcja służąca do obliczenia k_3
+double rk4_k3(double lambda, double y, double dt, double k_2) {
+    return lambda * (y + dt * k_2 / 2.0);
+}
 
-    //robimy przerwę w pliku
-    fprintf(filename, " \n \n");
+//funkcja służąca do obliczenia k_4
+double rk4_k4(double lambda, double y, double dt, double k_3) {
+    return lambda * (y + dt * k_3);
 }
 
 // Zadanie 4. //////////////////////// RLC /////////////////////////////
 
-double V_t(double om_v, double t) {
-    return 10.0 * sin(om_v * t);
-}
+void ex4_rlc(double freq, FILE* filenameQ, FILE* filenameI, double dt) {
 
-double g(double t, double Q, double I, double om_v, double R, double L, double C) {
-    return (V_t(om_v, t) / L) - (R * I / L) - (Q / (L * C));
-}
-
-void ex4_rlc(double freq, FILE* filename, double dt) {
     double R = 100;
     double L = 0.1;
     double C = 0.001;
@@ -176,15 +195,12 @@ void ex4_rlc(double freq, FILE* filename, double dt) {
     double om_0 = 1.0 / sqrt(L * C);
     double T_0 = 2.0 * M_PI / om_0;
 
-    double t_min = 0.0;
-    double t_max = 4 * T_0;
-
     double Q = 0.0;
     double I = 0.0;
 
     double om_v = freq * om_0;
 
-    for (double t = t_min; t <= t_max; t += dt) {
+    for (double t = 0.; t <= 4 * T_0; t += dt) {
 
         double kq_1 = I;
         double ki_1 = g(t, Q, I, om_v, R, L, C);
@@ -201,76 +217,18 @@ void ex4_rlc(double freq, FILE* filename, double dt) {
         Q = Q + (dt / 6) * (kq_1 + (2 * kq_2) + (2 * kq_3) + kq_4);
         I = I + (dt / 6) * (ki_1 + (2 * ki_2) + (2 * ki_3) + ki_4);
         
-        fprintf(filename, "%f %f \n", t, Q);
+        fprintf(filenameQ, "%f %f \n", t, Q);
+        fprintf(filenameI, "%f %f \n", t, I);
     }
 
-    fprintf(filename, "\n \n");
+    fprintf(filenameQ, "\n \n");
+    fprintf(filenameI, "\n \n");
 }
 
-int main() {
+double V_t(double om_v, double t) {
+    return 10.0 * sin(om_v * t);
+}
 
-    FILE* solutions;
-
-    FILE* errors;
-
-    FILE* analytical;
-    
-    solutions = fopen("ex1_sol.dat", "w");
-    errors = fopen("ex1_err.dat", "w");
-    analytical = fopen("ex1_an.dat", "w");
-
-    //wywołanie funkcji dla zadania 1
-    ex1_euler(solutions, errors, 0.01);
-    ex1_euler(solutions, errors, 0.1);
-    ex1_euler(solutions, errors, 1.0);
-
-    ex1_euler_analytically(analytical, 0.01);
-
-    fclose(solutions);
-    fclose(errors);
-    fclose(analytical);
-
-    //wywołanie funkcji dla zadania 2
-    solutions = fopen("ex2_sol.dat", "w");
-    errors = fopen("ex2_err.dat", "w");
-    analytical = fopen("ex2_an.dat", "w");
-
-    ex2_rk2(solutions, errors, 0.01);
-    ex2_rk2(solutions, errors, 0.1);
-    ex2_rk2(solutions, errors, 1.0);
-
-    ex2_rk2_analytically(analytical, 0.01);
-
-    fclose(solutions);
-    fclose(errors);
-    fclose(analytical);
-
-    //wywołanie funkcji dla zadania 3
-    solutions = fopen("ex3_sol.dat", "w");
-    errors = fopen("ex3_err.dat", "w");
-    analytical = fopen("ex3_an.dat", "w");
-
-    ex3_rk4(solutions, errors, 0.01);
-    ex3_rk4(solutions, errors, 0.1);
-    ex3_rk4(solutions, errors, 1.0);
-
-    ex3_rk4_analytically(analytical, 0.01);
-
-    fclose(solutions);
-    fclose(errors);
-    fclose(analytical);
-
-    //wywołanie funkcji dla zadania 4
-    FILE* rlc;
-
-    rlc = fopen("ex4_sol.dat", "w");
-    
-    double dt = pow(10.0, -4.0);
-
-    ex4_rlc(0.5, rlc, dt);
-    ex4_rlc(0.8, rlc, dt);
-    ex4_rlc(1.0, rlc, dt);
-    ex4_rlc(1.2, rlc, dt);
-
-    fclose(rlc);
+double g(double t, double Q, double I, double om_v, double R, double L, double C) {
+    return (V_t(om_v, t) / L) - (R * I / L) - (Q / (L * C));
 }
--- Lab 01 - RRZ - metoda Eulera, RK2, RK4/Wykresy/gnuplot.sh
+++ Lab 01 - RRZ - metoda Eulera, RK2, RK4/Wykresy/gnuplot.sh
@@ -1,74 +1,59 @@
-#!/usr/bin/gnuplot
-
 set term png
-set grid
 
 #### ZADANIE 1 ####################################################
 
 set output "ex1_sol.png"
 set xlabel "t"
 set ylabel "y(t)"
-set title "Metoda Eulera"
-plot "ex1_sol.dat" i 0 using 1:2 w l t "y num dt= 0.01", "ex1_sol.dat" i 1 using 1:2 w p t "y num dt= 0.1", "ex1_sol.dat" i 2 using 1:2 w p t "y num dt= 1.0"
+set title "Metoda Eulera - rozwiązanie"
+plot "ex1_sol.txt" i 0 using 1:2 w p lw 1 pt 1 t "dt = 0.01", "ex1_sol.txt" i 1 using 1:2 w p pt 2 t "dt = 0.1", "ex1_sol.txt" i 2 using 1:2 w p lw 1 pt 6 t "dt = 1.0", "ex1_an.txt" i 0 using 1:2 w l lt 2 t "analitycznie" 
 
 set output "ex1_err.png"
 set xlabel "t"
-set ylabel "y(t)"
-set title "Metoda Eulera"
-plot "ex1_err.dat" i 0 using 1:2 w l t "dt = 0.01", "ex1_err.dat" i 1 using 1:2 w p t "dt = 0.1", "ex1_err.dat" i 2 using 1:2 w p t "dt = 1.0"
-
-set output "ex1_an.png"
-set xlabel "t"
-set ylabel "y(t)"
-set title "Metoda Eulera"
-plot "ex1_an.dat" i 0 using 1:2 w l t "dt = 0.01"
+set ylabel "y_n(t) - y_a(t)"
+set title "Metoda Eulera - błąd globalny"
+plot "ex1_err.txt" i 0 using 1:2 w p lw 1 pt 1 t "dt = 0.01", "ex1_err.txt" i 1 using 1:2 w p lw 1 pt 2 t "dt = 0.1", "ex1_err.txt" i 2 using 1:2 w l lw 1 lt 3 t "dt = 1.0"
 
 #### ZADANIE 2 ####################################################
 
 set output "ex2_sol.png"
 set xlabel "t"
 set ylabel "y(t)"
-set title "Metoda RK2"
-plot "ex2_sol.dat" i 0 using 1:2 w l t "y num dt= 0.01", "ex2_sol.dat" i 1 using 1:2 w p t "y num dt= 0.1", "ex2_sol.dat" i 2 using 1:2 w p t "y num dt= 1.0"
+set title "Metoda RK2 - rozwiązanie"
+plot "ex2_sol.txt" i 0 using 1:2 w p lw 1 pt 1 t "dt = 0.01", "ex2_sol.txt" i 1 using 1:2 w p lw 1 pt 2 t "dt = 0.1", "ex2_sol.txt" i 2 using 1:2 w p lw 1 pt 6 t "dt = 1.0", "ex1_an.txt" i 0 using 1:2 w l lt 2 t "analitycznie"
 
 
 set output "ex2_err.png"
 set xlabel "t"
-set ylabel "y(t)"
-set title "Metoda RK2"
-plot "ex2_err.dat" i 0 using 1:2 w l lw 2 t "dt = 0.01", "ex2_err.dat" i 1 using 1:2 w p t "dt = 0.1", "ex2_err.dat" i 2 using 1:2 w p t "dt = 1.0"
-
-set output "ex2_an.png"
-set xlabel "t"
-set ylabel "y(t)"
-set title "Metoda RK2"
-plot "ex2_an.dat" i 0 using 1:2 w l t "dt = 0.01"
+set ylabel "y_n(t) - y_a(t)"
+set title "Metoda RK2 - błąd globalny"
+plot "ex2_err.txt" i 0 using 1:2 w p lw 1 pt 1 t "dt = 0.01", "ex2_err.txt" i 1 using 1:2 w p lw 1 pt 2 t "dt = 0.1", "ex2_err.txt" i 2 using 1:2 w l lw 1 lt 3 t "dt = 1.0"
 
 #### ZADANIE 3 ####################################################
 
 set output "ex3_sol.png"
 set xlabel "t"
 set ylabel "y(t)"
-set title "Metoda RK4"
-plot "ex3_sol.dat" i 0 using 1:2 w l lw 2 t "y num dt= 0.01", "ex3_sol.dat" i 1 using 1:2 w p t "y num dt= 0.1", "ex3_sol.dat" i 2 using 1:2 w p t "y num dt= 1.0"
+set title "Metoda RK4 - rozwiązanie"
+plot "ex3_sol.txt" i 0 using 1:2 w p lw 1 pt 1 t "dt = 0.01", "ex3_sol.txt" i 1 using 1:2 w p lw 1 pt 2 t "dt = 0.1", "ex3_sol.txt" i 2 using 1:2 w p lw 1 pt 6 t "dt = 1.0", "ex1_an.txt" i 0 using 1:2 w l lt 2 t "analitycznie"
 
 
 set output "ex3_err.png"
 set xlabel "t"
-set ylabel "y(t)"
-set title "Metoda RK4"
-plot "ex3_err.dat" i 0 using 1:2 w l lw 2 t "dt = 0.01", "ex3_err.dat" i 1 using 1:2 w p t "dt = 0.1", "ex3_err.dat" i 2 using 1:2 w p t "dt = 1.0"
-
-set output "ex3_an.png"
-set xlabel "t"
-set ylabel "y(t)"
-set title "Metoda RK2"
-plot "ex3_an.dat" i 0 using 1:2 w l t "dt = 0.01"
+set ylabel "y_n(t) - y_a(t)"
+set title "Metoda RK4 - błąd globalny"
+plot "ex3_err.txt" i 0 using 1:2 w p lw 1 pt 1 t "dt = 0.01", "ex3_err.txt" i 1 using 1:2 w p lw 1 pt 2 t "dt = 0.1", "ex3_err.txt" i 2 using 1:2 w l lw 1 lt 3 t "dt = 1.0"
 
 #### ZADANIE 4 ####################################################
 
-set output "ex4_sol.png"
+set output "ex4_Q.png"
 set xlabel "t"
 set ylabel "Q(t)"
 set title "Q(t) metoda RK4"
-plot "ex4_sol.dat" i 0 using 1:2 w l t "omega = 0.5omega_0", "ex4_sol.dat" i 1 using 1:2 w l t "omega = 0.8omega_0","ex4_sol.dat" i 2 using 1:2 w l t "omega = 1,0omega_0","ex4_sol.dat" i 3 using 1:2 w l t "omega = 1.2omega_0"
+plot "ex4_Q.txt" i 0 using 1:2 w l lw 1 t "0.5 om_0", "ex4_Q.txt" i 1 using 1:2 w l lw 1 t "0.8 om_0","ex4_Q.txt" i 2 using 1:2 w l lw 1 t "1.0 om_0","ex4_Q.txt" i 3 using 1:2 w l lw 1 t "1.2 om_0"
+
+set output "ex4_I.png"
+set xlabel "t"
+set ylabel "I(t)"
+set title "I(t) metoda RK4"
+plot "ex4_I.txt" i 0 using 1:2 w l lw 1 t "0.5 om_0", "ex4_I.txt" i 1 using 1:2 w l lw 1 t "0.8 om_0","ex4_I.txt" i 2 using 1:2 w l lw 1 t "1.0 om_0","ex4_I.txt" i 3 using 1:2 w l lw 1 t "1.2 om_0"
